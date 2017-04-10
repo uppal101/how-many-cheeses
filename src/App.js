2dc0d1c3b8d22234637507ec5911ede3961743ad
Sanjeet Uppal <sanjeet.uppal92@gmail.com>
2017-04-09 22:08:04 -0700

    search button created

--- src/App.js
+++ src/App.js
@@ -1,40 +1,34 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Map from './components/GoogleMap/GoogleMap'
 import Places from './components/PlacesComponent/Places'
-
-// import SideNavSubCategories from './components/SideNav/SideNavSubCategories'
-// import SideNav from './components/SideNav/SideNav'
-
 import SideNav from './components/SideNav/SideNav'
 import NavBar from './components/Common/Navigation/NavBar'
-
+import SearchButton from './components/Common/Button'
 
 class App extends Component {
-  render() {
-   const location = {
-    lat: 40.7575285,
-    lng: -73.9884469
-   }
-    return (
-
-
-        <div style={{width:400, height:400, background:'red'}}>
-
-         <div>
-
-          <NavBar/>
-         </div>
-        <div style={{width:500, height:500, background:'red'}}>
-          <SideNav/>
-          <Map center={location} />
-
-        </div>
-
-
-      </div>
-    );
-  }
+    render() {
+        const location = {
+            lat: 40.7575285,
+            lng: -73.9884469
+        }
+        return (
+            <div>
+                <div>
+                    <NavBar/>
+                </div>
+                <div style={{
+                    width: 500,
+                    height: 500,
+                    background: 'red'
+                }}>
+                    <SideNav/>
+                    <Map center={location}/>
+                    <SearchButton/>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/Common/Button.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react"
+
+class SearchButton extends Component {
+  render() {
+    return (
+       <button type="submit">Search</button>
+    );
+  }
+}
+
+export default SearchButton
