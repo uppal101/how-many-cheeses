bd57277890f3bba54a8e40760e0991a7ed4a0903
Gurdip Singh <gurdipsingh1990@gmail.com>
2017-04-09 18:47:13 -0700

    Added SideNav Components with properply working sidenav

--- src/components/SideNav/SideNav.js
+++ src/components/SideNav/SideNav.js
@@ -1,18 +1,29 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import Button from 'react-bootstrap/lib/Button';
-// import MenuItem from 'react-bootstrap/lib/Button';
-import {NavDropdown, MenuItem} from 'react-bootstrap';
+import SideNavCategories from './SideNavCategories'
+import {ButtonGroup, DropdownButton, MenuItem, Button}  from 'react-bootstrap';
 
-const SideNavSubCategories = () => {
+const SideNav = () => {
   return (
-    <div>
-    <MenuItem eventKey="1">Search By Firmness</MenuItem>
-    <MenuItem eventKey="2">Search By Animal</MenuItem>
-    <MenuItem eventKey="3">Search By Cheese Name</MenuItem>
-    <MenuItem eventKey="4">Search For Substitute</MenuItem>
-</div>
+
+    <ButtonGroup vertical>
+
+      <Button>See All Cheeses</Button>
+      <DropdownButton title="Search" id="bg-vertical-dropdown-1">
+        <MenuItem eventKey="1">Search By Firmness</MenuItem>
+        <MenuItem eventKey="2">Search By Animal</MenuItem>
+        <MenuItem eventKey="3">Search By Cheese Name</MenuItem>
+        <MenuItem eventKey="4">Search For Substitute</MenuItem>
+      </DropdownButton>
+      <Button>Find in Stores</Button>
+      <Button>Give Me That Random Cheese!</Button>
+      <Button>See My Favoritessss</Button>
+
+    </ButtonGroup>
+
+
+
   )
 }
 
-export default SideNavSubCategories;
+export default SideNav
