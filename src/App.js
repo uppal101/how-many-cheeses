b6c947ae7096fc3e442f9f97636d6a36d736f829
Gurdip Singh <gurdipsingh1990@gmail.com>
2017-04-09 18:23:53 -0700

    Add GoogleMap component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Map from './components/GoogleMap/GoogleMap'
+import Places from './components/PlacesComponent/Places'
+// import SideNavSubCategories from './components/SideNav/SideNavSubCategories'
+// import SideNavCategories from './components/SideNav/SideNavCategories'
 
 class App extends Component {
   render() {
+   const location = {
+    lat: 0,
+    lng: 0
+   }
     return (
-      <div className="App">
-        <div className="App-header">
+      <div >
+        <div style={{width:500, height:500, background:'red'}}>
+          <Map center={location} />
         </div>
-          <h2>Let's get started</h2>
+
+
       </div>
     );
   }
---
+++ src/components/GoogleMap/GoogleMap.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import {GoogleMapLoader, GoogleMap, Marker} from 'react-google-maps'
+// const API_KEY = 'AIzaSyA6_Q41cfo5GrTNCxzbKEIBg2lE7Gn-acw';
+
+class Map extends Component {
+ render () {
+  const mapContainer = <div>style={{height: '100%', width: '100%'}}</div>
+  return (
+   <div>Hello</div>
+  )
+ }
+}
+
+export default Map
