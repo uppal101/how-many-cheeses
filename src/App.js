53c8f1e56a00a19c191d8b03eadce22e54149286
Sanjeet Uppal <sanjeet.uppal92@gmail.com>
2017-04-10 22:44:50 -0700

    tried to importone component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, {Component} from 'react';
 import './App.css';
-import { BrowerRouter as Router } from 'react-router-dom'
+import { BrowerRouter as Router, Route, link } from 'react-router-dom'
+import SignInPage from './components/Login/SignInPage'
 // import Map from './components/GoogleMap/GoogleMap'
 // import Places from './components/PlacesComponent/Places'
-import SideNav from './components/SideNav/SideNav'
-import NavBar from './components/Common/Navigation/NavBar'
-import SearchButton from './components/Common/Button'
-import SearchBar from './components/Common/SearchBar'
-import TextArea from './components/TextArea/TextArea'
-import Header from './components/Header'
-import LoginTitle from './components/Login/LoginTitle'
-import SignIn from './components/Login/SignIn'
-import FavoritesTable from './components/favoritesTable/Table'
-import ResultsTable from './components/SearchTableResults/Table'
-import StoreTable from './components/StoreTable/Table'
+// import SideNav from './components/SideNav/SideNav'
+// import NavBar from './components/Common/Navigation/NavBar'
+// import SearchButton from './components/Common/Button'
+// import SearchBar from './components/Common/SearchBar'
+// import TextArea from './components/TextArea/TextArea'
+// import Header from './components/Header'
+// import LoginTitle from './components/Login/LoginTitle'
+// import SignIn from './components/Login/SignIn'
+// import FavoritesTable from './components/favoritesTable/Table'
+// import ResultsTable from './components/SearchTableResults/Table'
+// import StoreTable from './components/StoreTable/Table'
 
 class App extends Component {
     render() {
@@ -39,10 +40,10 @@ class App extends Component {
                     <li><link to= '/Favorites'>Favorites</link></li>
                   </ul>
 
-                  <Route path='/Sign-in' component={SignIn}/>
-                  <Route path='/Search' component={Search}/>
+                  <Route path='/Sign-in' component={SignInPage}/>
+                  {/* <Route path='/Search' component={Search}/>
                   <Route path='/FindAStore' component={Store}/>
-                  <Route path='/Favorites' component={Store}/>
+                  <Route path='/Favorites' component={Favorites}/> */}
                     {/* <NavBar/>
                 </div>
                 <div>
